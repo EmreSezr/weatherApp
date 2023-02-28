3f080dfe5e0ee8d98f31f099697cb0195b03ca45
Emre Sezer <1997.emresezer@gmail.com>
2023-02-28 18:27:57 +0300

    first commit

---
+++ weather-app/src/App.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+function App() {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState([]);
+  const API_KEY = "c4389769c0dc4abe874133129232802";
+  const handleInputChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+   
+    event.preventDefault();
+    setWeatherData([]);
+    fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`
+    )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Şehir bulunamadı!");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      setWeatherData((prevData) => {
+        const newData = [...prevData, data].slice(-1);
+        return newData;
+      });
+      setCity("");
+      })
+      .catch((error) => {
+        alert("Şehir bulunamadı. Geçerli şehir ismi giriniz.");
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="city">Şehir İsmi:</label>
+        <input
+          type="text"
+          id="city"
+          name="city"
+          placeholder="Şehir..."
+          value={city}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Gönder</button>
+      </form>
+      {weatherData.map((data) => (
+        <Weather key={data.location.name} data={data} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ weather-app/src/Weather.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Weather(props) {
+  const { location, current } = props.data;
+  return (
+    <div className="Weather">
+      <h2>
+        Hava Durumu: {location.name}
+      </h2>
+      <img src={current.condition.icon} alt={current.condition.text} />
+      <p>Sıcaklık: {current.temp_c}°C </p>
+      <p>Durum: {current.condition.text}</p>
+      <p>Rüzgar: {current.wind_kph} km/h</p>
+      <p>Nem: {current.humidity}%</p>
+      <p>Hissedilen Derece: {current.feelslike_c}°C</p>
+    </div>
+  );
+}
+
+export default Weather;
