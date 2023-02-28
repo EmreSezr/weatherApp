2db24fda0e7270a044428d697d9f5aec311db152
Emre Sezer <1997.emresezer@gmail.com>
2023-02-28 21:58:57 +0300

    d

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -11,24 +11,23 @@ function App() {
   };
 
   const handleFormSubmit = (event) => {
-   
     event.preventDefault();
     setWeatherData([]);
     fetch(
       `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`
     )
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Şehir bulunamadı!");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      setWeatherData((prevData) => {
-        const newData = [...prevData, data].slice(-3);
-        return newData;
-      });
-      setCity("");
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Şehir bulunamadı!");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setWeatherData((prevData) => {
+          const newData = [...prevData, data].slice(-3);
+          return newData;
+        });
+        setCity("");
       })
       .catch((error) => {
         alert("Şehir bulunamadı. Geçerli şehir ismi giriniz..");
@@ -38,24 +37,24 @@ function App() {
 
   return (
     <div className="all">
-    <div className="App">
-      <h1>Weather App</h1>
-      <form onSubmit={handleFormSubmit}>
-        <label htmlFor="city">Şehir İsmi:</label>
-        <input
-          type="text"
-          id="city"
-          name="city"
-          placeholder="Şehir..."
-          value={city}
-          onChange={handleInputChange}
-        />
-        <button type="submit">Gönder</button>
-      </form>
-      {weatherData.map((data) => (
-        <Weather key={data.location.name} data={data} />
-      ))}
-    </div>
+      <div className="App">
+        <h1>Weather App</h1>
+        <form onSubmit={handleFormSubmit}>
+          <label htmlFor="city">Şehir İsmi:</label>
+          <input
+            type="text"
+            id="city"
+            name="city"
+            placeholder="Şehir..."
+            value={city}
+            onChange={handleInputChange}
+          />
+          <button type="submit">Gönder</button>
+        </form>
+        {weatherData.map((data) => (
+          <Weather key={data.location.name} data={data} />
+        ))}
+      </div>
     </div>
   );
 }
--- weather-app/src/Weather.js
+++ weather-app/src/Weather.js
@@ -10,7 +10,7 @@ function Weather(props) {
       <img src={current.condition.icon} alt={current.condition.text} />
       <p>Sıcaklık: {current.temp_c}°C </p>
       <p>Durum: {current.condition.text}</p>
-      <p>Rüzgar: {current.wind_kph} km/h</p>
+      <p>Rüzgar: {current.wind_kph} km/s</p>
       <p>Nem: {current.humidity}%</p>
       <p>Hissedilen Derece: {current.feelslike_c}°C</p>
     </div>
