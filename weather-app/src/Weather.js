86a710b1cb56bd3f9d6f40195c7e1efc40a4f345
Emre Sezer <1997.emresezer@gmail.com>
2023-03-03 16:42:58 +0300

    Style changes added & Weather search result box and box-color added

--- weather-app/src/Weather.js
+++ weather-app/src/Weather.js
@@ -1,18 +1,19 @@
 import React from "react";
+import "./Weather.css";
 
 function Weather(props) {
   const { location, current } = props.data;
   return (
     <div className="Weather">
-      <h2>
-        Hava Durumu: {location.name}
-      </h2>
+      <h2>Hava Durumu: {location.name}</h2>
       <img src={current.condition.icon} alt={current.condition.text} />
-      <p>Sıcaklık: {current.temp_c}°C </p>
-      <p>Durum: {current.condition.text}</p>
-      <p>Rüzgar: {current.wind_kph} km/s</p>
-      <p>Nem: {current.humidity}%</p>
-      <p>Hissedilen Derece: {current.feelslike_c}°C</p>
+      <div className="Weather-Results">
+        <p>Sıcaklık: {current.temp_c}°C </p>
+        <p>Durum: {current.condition.text}</p>
+        <p>Rüzgar: {current.wind_kph} km/s</p>
+        <p>Nem: {current.humidity}%</p>
+        <p>Hissedilen Derece: {current.feelslike_c}°C</p>
+      </div>
     </div>
   );
 }
